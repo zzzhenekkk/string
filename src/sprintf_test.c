3b660bcd6d4652e5cf4384f33de076f29e6797ca
Evgeniy Zligostev <89052742425@mail.ru>
2023-11-08 22:48:04 +0300

    тесты на ширину и точность

--- src/sprintf_test.c
+++ src/sprintf_test.c
@@ -2,10 +2,20 @@
 
 int main() {
   static char buf1[500] = {0};
+  static char buf3[500] = {0};
+  static char buf4[500] = {0};
   static char buf2[500] = {0};
-  sprintf(buf1, "Hello %d \n", 123);
+
+
+  
+  sprintf(buf3, "%d 7d '%7d' .7d '%.7d' 3.7d '%3.7d' 10.7d '%10.7d'\n", 123, 123, 123, 123, 123, 123, 123, 123, 123);
+  
+  sprintf(buf1, "%d 6d '%6d' 6.10d '%6.10d' -.10d '%-.10d' +.10d '%+.10d'\n", 123, 123, 123, 123, 123);
+  
   s21_sprintf(buf2, "Hello2 %+- #0*.*ld\n", 555, 667);
 
+printf("%s", buf4);
+  printf("%s", buf3);
   printf("%s", buf1);
   printf("%s", buf2);
 }
